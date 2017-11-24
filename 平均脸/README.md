02a92935c7d6393c5fa1317f6d0d961d156b65b4
mattzheng <mattzheng@yeah.net>
2017-11-24 11:09:36 +0800

    Add files via upload

---
+++ 平均脸/README.md
@@ -0,0 +1,5 @@
+在完成各个library的安装后。
+第一步：将要平均的照片放入faces文档，确保图片为jpg格式。
+第二步：在终端运行 python face_landmark_detection.py shape_predictor_68_face_landmarks.dat faces，并在程序运行结束后将所有faces文档中的文件复制到presidents文档中（如无法完成dlib安装，可略过该步骤，直接用文摘菌提供的素材）
+第三步：在终端运行 python faceAverage.py 
+这样就能看到制作成功的平均脸了！
---
+++ 平均脸/faceAverage.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+#
+#   This example program shows how to find frontal human faces in an image and
+#   estimate their pose.  The pose takes the form of 68 landmarks.  These are
+#   points on the face such as the corners of the mouth, along the eyebrows, on
+#   the eyes, and so forth.
+#
+#   The face detector we use is made using the classic Histogram of Oriented
+#   Gradients (HOG) feature combined with a linear classifier, an image pyramid,
+#   and sliding window detection scheme.  The pose estimator was created by
+#   using dlib's implementation of the paper:
+#      One Millisecond Face Alignment with an Ensemble of Regression Trees by
+#      Vahid Kazemi and Josephine Sullivan, CVPR 2014
+#   and was trained on the iBUG 300-W face landmark dataset (see
+#   https://ibug.doc.ic.ac.uk/resources/facial-point-annotations/):  
+#      C. Sagonas, E. Antonakos, G, Tzimiropoulos, S. Zafeiriou, M. Pantic. 
+#      300 faces In-the-wild challenge: Database and results. 
+#      Image and Vision Computing (IMAVIS), Special Issue on Facial Landmark Localisation "In-The-Wild". 2016.
+#   You can get the trained model file from:
+#   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2.
+#   Note that the license for the iBUG 300-W dataset excludes commercial use.
+#   So you should contact Imperial College London to find out if it's OK for
+#   you to use this model file in a commercial product.
+#
+#
+#   Also, note that you can train your own models using dlib's machine learning
+#   tools. See train_shape_predictor.py to see an example.
+#
+#
+# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
+#   You can install dlib using the command:
+#       pip install dlib
+#
+#   Alternatively, if you want to compile dlib yourself then go into the dlib
+#   root folder and run:
+#       python setup.py install
+#   or
+#       python setup.py install --yes USE_AVX_INSTRUCTIONS
+#   if you have a CPU that supports AVX instructions, since this makes some
+#   things run faster.  
+#
+#   Compiling dlib should work on any operating system so long as you have
+#   CMake and boost-python installed.  On Ubuntu, this can be done easily by
+#   running the command:
+#       sudo apt-get install libboost-python-dev cmake
+#
+#   Also note that this example requires scikit-image which can be installed
+#   via the command:
+#       pip install scikit-image
+#   Or downloaded from http://scikit-image.org/download.html. 
+
+import sys
+import os
+import dlib
+import glob
+import numpy as np
+from skimage import io
+
+def shape_to_np(shape):
+    # initialize the list of (x, y)-coordinates
+    coords = np.zeros((68, 2), dtype = int)
+ 
+    # loop over the 68 facial landmarks and convert them
+    # to a 2-tuple of (x, y)-coordinates
+    for i in range(0, 68):
+        coords[i] = (int(shape.part(i).x), int(shape.part(i).y))
+ 
+    # return the list of (x, y)-coordinates
+    return coords
+
+if len(sys.argv) != 3:
+    print(
+        "Give the path to the trained shape predictor model as the first "
+        "argument and then the directory containing the facial images.\n"
+        "For example, if you are in the python_examples folder then "
+        "execute this program by running:\n"
+        "python face_landmark_detection.py shape_predictor_68_face_landmarks.dat faces\n"
+        "You can download a trained facial shape predictor from:\n"
+        "    http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2")
+    exit()
+
+predictor_path = sys.argv[1]
+faces_folder_path = sys.argv[2]
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+
+for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):
+    print("Processing file: {}".format(f))
+    img = io.imread(f)
+
+
+    # Ask the detector to find the bounding boxes of each face. The 1 in the
+    # second argument indicates that we should upsample the image 1 time. This
+    # will make everything bigger and allow us to detect more faces.
+    dets = detector(img, 1)
+
+    #print("Number of faces detected: {}".format(len(dets)))
+    for k, d in enumerate(dets):
+        # # Get the landmarks/parts for the face in box d.
+        shape = predictor(img, d)
+        shape_np = shape_to_np(shape)
+        np.savetxt(f + '.txt', shape_np, fmt = '%i')
